04e5d0f4474e0bd256d2bcedc91610a7310cfffe
z-burke <zachb89@gmail.com>
2021-08-30 15:46:40 -0500

    Modify name of functions and add a few more.

--- AFSDK.psm1
+++ AFSDK.psm1
@@ -5,7 +5,7 @@ if (
     throw "OSIsoft.AFSDK assembly not found." # TODO: Make this a missing assembly reference.
 }
 
-function Get-AfAnalysisInputPiPoints {
+function Get-AfAnalysesInputPiPoints {
     # Returns a list of PI Points that act as inputs for a list of Analyses.
     [CmdletBinding()]
     Param(
@@ -16,10 +16,10 @@ function Get-AfAnalysisInputPiPoints {
     $InputAfAttributes = $AfAnalyses.AnalysisRule.GetConfiguration().GetInputs()
     # TODO: Ensure no duplicates are returned
     $PiPoints = $InputAfAttributes.PIPoint.Where({$_ -ne $null})
-    return [OSIsoft.AF.PI.PIPoints]$PiPoints
+    return [OSIsoft.AF.PI.PIPoint[]]$PiPoints
 }
 
-function Get-AfAnalysisOutputPiPoints {
+function Get-AfAnalysesOutputPiPoints {
     # Returns a list of PI Points that receive data from a list of Analyses.
     [CmdletBinding()]
     Param(
@@ -30,13 +30,24 @@ function Get-AfAnalysisOutputPiPoints {
     $InputAfAttributes = $AfAnalyses.AnalysisRule.GetConfiguration().GetOutputs()
     # TODO: Ensure no duplicate PI Points are returned
     $PiPoints = $InputAfAttributes.PIPoint.Where({$_ -ne $null})
-    return [OSIsoft.AF.PI.PIPoints]$PiPoints
+    return [OSIsoft.AF.PI.PIPoint[]]$PiPoints
 }
 
 function Get-AllAfAnalysesFromAfServer {}
 
-function Get-OpenAFEventFrames {}
+function Get-OpenAFEventFrames {
+    # From an AF Database or from an AF Server (use parameter sets), retrieve an array of EventFrames that are open.
+}
+
+function Stop-EventFrames {
+    # Set the current time as the end time for a list of AF EventFrames.
+}
 
 function Get-PiVisionDisplayAfAttributes {}
 
 function Get-PiPointProperties {}
+
+function Convert-ObjectsToPsCustomObjects{
+    # Generically converts Objects to PSCustom Objects to enable use with PowerShell functions like Export-Csv and Out-Gridview.
+}
+
