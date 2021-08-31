bf435b283ceb7197827b1786213f463f52eb08f8
z-burke <zachb89@gmail.com>
2021-08-31 12:51:45 -0500

    Add example New-CsvOfAllAfAnalysesAndOutputPiPoints

--- Examples/Get-InputPiPointsForAllAfAnalysesOnAfServer.ps1
+++
@@ -1,9 +0,0 @@
-using Assembly OSIsoft.AFSDK
-
-Import-Module ".\AFSDK.psm1"
-
-# TODO: Get all Analyses
-
-$AfAnalysis = [OSIsoft.AF.AFObject]::FindObject("\\GRD001P-APP029\PI System Health Monitoring (Radix)\Test\Analyses[Analysis2]")
-
-[OSIsoft.AF.Analysis.AFAnalysis[]]$AFAnalysis | Get-AfAnalysesInputPiPoints
